9ecc2a312d315479f4e827a3b0ba959eee058d00
s1f10250205 <s1f102502058@iniad.org>
2025-07-02 21:42:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
+import re
+from flask import Markup
+
 
 kinoko_count = 3
 takenoko_count = 5
@@ -17,3 +20,20 @@ def answer():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+def linkify(text):
+    url_pattern = r'(https?://[^\s]+)'
+    return Markup(re.sub(url_pattern, r'<a href="\\1" target="_blank">\\1</a>', text))
+
+@app.route('/vote', methods=['POST'])
+def answer():
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
+
+    messages_with_links = [linkify(m) for m in messages]
+    return render_template('vote.html', messages=messages_with_links,
+                           kinoko_count=kinoko_count,
+                           takenoko_count=takenoko_count,
+                           kinoko_percent=kinoko_percent,
+                           takenoko_percent=takenoko_percent)
+
